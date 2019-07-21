d4725efc2644e529c7d30520d9d57a1037e62555
Amgalan <darzhaev@gmail.com>
2019-07-21 09:35:13 +0300

    Update passcard_info_view.py
    Стали отображаться в реальном времени дата посещения, посетитель, признак подозрительности, продолжительность пребывания

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,21 +1,22 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
-
+from datacenter.models import is_visit_long
+from datacenter.models import get_time
+from datacenter.models import format_time
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
+    passcard = Passcard.objects.filter(passcode=passcode)
 
     this_passcard_visits = [
         {
-            "entered_at": "11-04-2018",
-            "duration": "25:03",
-            "is_strange": False
-        },
+            "entered_at": visit.entered_at,
+            "duration": format_time(get_time(visit.leaved_at) - visit.entered_at),
+            "is_strange": is_visit_long(visit)
+        } for visit in Visit.objects.filter(passcard=passcard),
     ]
     context = {
-        "passcard": passcard,
+        "passcard": passcard[0],
         "this_passcard_visits": this_passcard_visits
     }
     return render(request, 'passcard_info.html', context)
