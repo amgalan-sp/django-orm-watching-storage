12b25d18176353da41ef7df2e7f7926238ed0726
Amgalan <darzhaev@gmail.com>
2019-07-21 09:38:05 +0300

    Update storage_information_view.py
    Стали отображаться в реальном времени имя посетителя, продолжительность пребывания, добавлена функция признака подозрительности

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,17 +1,18 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
-
+from datacenter.models import is_visit_long
+from datacenter.models import get_time
+from datacenter.models import format_time
 
 def storage_information_view(request):
-    # Программируем здесь
-
     non_closed_visits = [
         {
-            "who_entered": "Richard Shaw",
-            "entered_at": "11-04-2018 25:34",
-            "duration": "25:03",
-        }
+            "who_entered": visit.passcard,
+            "entered_at": visit.entered_at,
+            "duration": format_time(get_time(visit.leaved_at) - visit.entered_at),
+            "is_strange": is_visit_long(visit)            
+        } for visit in Visit.objects.filter(leaved_at=None)
     ]
     context = {
         "non_closed_visits": non_closed_visits,  # не закрытые посещения
