ba7497d68882aa71cd2ec02c176058d9451bc6ed
amgalan-sp <darzhaev@gmail.com>
2019-12-03 22:17:13 +0900

    Cleaned excess function

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -6,11 +6,12 @@ from datacenter.models import get_time
 from datacenter.models import format_time
 
 def storage_information_view(request):
+    
     non_closed_visits = [
         {
             "who_entered": visit.passcard,
             "entered_at": visit.entered_at,
-            "duration": format_time(get_time(visit.leaved_at) - visit.entered_at),
+            "duration": format_time(timezone.now() - visit.entered_at),
             "is_strange": is_visit_long(visit)            
         } for visit in Visit.objects.filter(leaved_at=None)
     ]
